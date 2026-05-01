ca87df7b28aeacce2d932ea2b7fec0e30c387b83
jaipooja <jaipooja199@gmail.com>
2026-05-01 13:43:35 +0200

    create userlist

--- Backend/app.js
+++ Backend/app.js
@@ -15,41 +15,61 @@ app.use(express.static(path.join(__dirname, "../public")));
 // Create an HTTP server
 const server = http.createServer(app);
 // Create a Socket.IO server and attach it to the HTTP server
-const io = new Server(server ,{
-   cors: {
+const io = new Server(server, {
+  cors: {
     origin: "http://localhost:3000",
   }
 }
-  
-);
 
-// Basic route
+);
 app.get("/", (req, res) => {
   //res.send("Hello from Express + Socket.IO");
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // Socket logic
+const rooms = {};
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
   // join room 
-  socket.on("joinRoom", (room) => {
+  socket.on("joinRoom", ({ room, username }) => {
+    socket.username = username;
+    socket.room = room;
     socket.join(room);
-    
+
+    if (!rooms[room]) {
+      rooms[room] = [];
+    }
+    rooms[room].push(username);
+
+    // send updated user list to room
+    io.to(room).emit("roomUsers", rooms[room]);
+
+
     //send message to everyone except the new user
-    socket.to(room).emit("message", `User ${socket.id} joined ${room}`);
+    socket.to(room).emit("message", ` NEW USER ${username} JOINED ${room}`);
   })
 
 
-  //send message to everyone ,, broadcast inside specific room only
+  //send message to everyone ,, broadcast inside selested room only
   socket.on("chatMessage", ({ room, message }) => {
-    io.to(room).emit("message", `(${socket.id}) : ${message}`);
+    io.to(room).emit("message", `(${socket.username}) : ${message}`);
 
   });
 
   socket.on("disconnect", () => {
-    console.log("user disconnected :", socket.id);
+    const { username, room } = socket;
+    if (room && rooms[room]) {
+      // remove user
+      rooms[room] = rooms[room].filter(u => u !== username);
+
+      // update list to everyone 
+      io.to(room).emit("roomUsers", rooms[room]);
+
+      // notify others
+      socket.to(room).emit("message", `${username} left`);
+    }
 
   })
 });
--- public/script.js
+++ public/script.js
@@ -1,16 +1,33 @@
 const socket = io("http://localhost:3000");
-
+let username = "";
 let currentRoom = "";
 
 function joinRoom() {
+    username = document.getElementById("usernameInput").value.trim();
     const room = document.getElementById("roomInput").value.trim();
-
+    if (!username) return alert("Enter a username");
     if (!room) return alert("Enter a room name");
 
     currentRoom = room;
-    socket.emit("joinRoom", room);
+    socket.emit("joinRoom", {
+        room,
+        username,
+    });
+
     document.getElementById("messageInput").focus();
 }
+
+socket.on("roomUsers", (users) => {
+  const usersList = document.getElementById("users");
+
+  usersList.innerHTML = ""; // clear old list
+
+  users.forEach((user) => {
+    const li = document.createElement("li");
+    li.textContent = user;
+    usersList.appendChild(li);
+  });
+});
 
 function sendMessage() {
     const input = document.getElementById("messageInput");
@@ -26,12 +43,13 @@ function sendMessage() {
 
     input.value = "";
 }
+
 socket.on("message", (msg) => {
     const chat = document.getElementById("chat");
     const div = document.createElement("div");
     div.textContent = msg;
     chat.appendChild(div);
-    chat.scrollTop = chat.scrollHeight;
+    chat.scrollTop = chat.scrollHeight; // Auto-scroll to the latest message
 });
 
 socket.on("connect", () => {
